0cab22ba9419c8d37a748e1dd2db99a0c3f361d5
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-13 23:16:35 +0300

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+handle session auth
+"""
+import base64
+from typing import Tuple, TypeVar
+from models.user import User
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """
+    class that handles session auth
+    """
+    pass

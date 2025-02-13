711219e4c5fd4bd5299652483ccac84d6d5a4470
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-13 21:22:37 +0300

    basicauth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,12 +16,11 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-
-if getenv('AUTH_TYPE') == 'auth':
+if getenv('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if getenv('AUTH_TYPE') == 'auth':
+elif getenv('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 

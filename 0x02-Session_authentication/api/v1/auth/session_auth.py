1217aa0ab696cddaa68f4020b7c81e082ce71fa3
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-13 23:32:05 +0300

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,9 @@
 """
 handle session auth
 """
-import base64
-from typing import Tuple, TypeVar
+import base64 import b64decode
+import uuid import uuid4
+from typing import Optional, Tuple, TypeVar
 from models.user import User
 from api.v1.auth.auth import Auth
 
@@ -14,14 +15,14 @@ class SessionAuth(Auth):
     """
     user_id_by_session_id = {}
 
-    def create_session(self, user_id: str = None) -> str:
+    def create_session(self, user_id: str = None) -> Optional[str]:
         """Creates a session id for the user.
         """
-        if type(user_id) is str:
-            session_id = str(uuid4())
-            self.user_id_by_session_id[session_id] = user_id
-            return session_id
-
+        if not user_id or not isinstance(user_id, str):
+            return
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Retrieves the user id of the user associated with

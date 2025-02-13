6e1f9cc564a426969000310c20f88e8b291afa92
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-13 22:03:43 +0300

    task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,21 +10,46 @@ class Auth:
     ''' A Class to manage the API authentication.
     '''
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(
+            self,
+            path: str,
+            excluded_paths: List[str]
+            ) -> bool:
         ''' Required auth
         '''
-        if path and not path.endswith('/'):
-            path = path + '/'
-
-        if not path or path not in excluded_paths:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        if not excluded_paths or excluded_paths == []:
-            return True
+        # handle * at end of excluded paths
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
 
         if path in excluded_paths:
             return False
-        return False
+
+        return True
+
 
     def authorization_header(self, request=None) -> Optional[str]:
         '''

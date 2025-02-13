a85bd1461ad2eafa98be62408712855dfd6d744d
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-14 00:23:59 +0300

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,10 @@
 handle session authentication
 """
 import uuid
-from typing import TypeVar
+from typing import TypeVar, Optional
 from models.user import User
 from api.v1.auth.auth import Auth
+from flask import request
 
 
 class SessionAuth(Auth):
@@ -31,3 +32,29 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> Optional[User]:
+        """
+	    Returns the User instance based on a session cookie
+	    """
+        if request is None:
+            return None
+        
+        session_id = request.cookies.get(self.session_cookie())
+        
+        if session_id is None:
+            return None
+        
+        user_id = self.user_id_for_session_id(session_id)
+        
+        if user_id is None:
+            return None
+        
+        user = User.get(user_id)
+        return user
+
+    def session_cookie(self):
+        """
+	    Returns the cookie name for the session (e.g., '_my_session_id')
+	    """
+        return "_my_session_id"

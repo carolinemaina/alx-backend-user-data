2ebce5590cab36676678ed3c986e9314b9c953a8
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2025-02-13 12:01:44 +0300

    task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,7 +25,7 @@ def not_found(error) -> str:
 def unauthorized(error) -> Tuple[str, int]:
     """ Unauthorized handler
     """
-    return jsonify({"error": "unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
